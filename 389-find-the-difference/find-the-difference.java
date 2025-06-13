bb83a6256f0e0e0c4ad0e1978c7312a82ddea969
Sidhant018 <sidhantanand18@gmail.com>
2025-06-14 02:26:31 +0530

    Time: 1 ms (99.75%) | Memory: 41.9 MB (63.61%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,6 +1,6 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        char[]ch=s.toCharArray();
+       /* char[]ch=s.toCharArray();
         char[]ch1=t.toCharArray();
         Arrays.sort(ch);
         Arrays.sort(ch1);
@@ -10,5 +10,15 @@ class Solution {
             }
         }
         return ch1[ch1.length-1];
+    }
+}*/char r=0;
+   for(char e:s.toCharArray()){
+    r^=e;//XOR krr rahe hai to find same characters  1 1 hoga toh 0 return krrta h XOR
+   }
+    for(char e:t.toCharArray()){
+    r^=e;
+    }
+    return r;
+
     }
 }
