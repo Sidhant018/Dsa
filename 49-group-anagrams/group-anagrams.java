34cdcaa26fdc414fa8b7cbb56ec814077db41bbc
Sidhant018 <sidhantanand18@gmail.com>
2025-07-31 00:01:13 +0530

    Time: 1412 ms (5.02%) | Memory: 47.9 MB (68.90%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,32 @@
+class Solution {
+    boolean isAnagram(String first ,String second){
+        if(first.length()!=second.length()){
+            return false;
+        }
+        char x[]=first.toCharArray();
+        char y[]=second.toCharArray();
+        Arrays.sort(x);
+        Arrays.sort(y);
+        return Arrays.equals(x,y);
+
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> bigList=new ArrayList<>();
+        for(String word:strs){
+            boolean added=false;//to check whether the word is already in the list or not
+            for(List<String> smallList:bigList){
+                if(isAnagram(smallList.get(0),word)){
+                    smallList.add(word);
+                    added=true;
+                    break;
+                }
+            }
+            if(!added){
+                List<String> newsmallList=new ArrayList<>();
+                newsmallList.add(word);
+                bigList.add(newsmallList);
+            }
+        }
+        return bigList;
+    }
+}
